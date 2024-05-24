af7cdd22d59ad218b30733af2a29f3cd43271c5f
Marco Zeuli <>
2024-05-24 12:14:28 +0200

    removed unused functions

--- force-app/main/default/lwc/pathAssistant/utils.js
+++ force-app/main/default/lwc/pathAssistant/utils.js
@@ -192,26 +192,3 @@ export class Step {
         return !!this.value;
     }
 }
-
-/**
- * Returns the object's master record type id
- * @param {Object} objectInfo Object metadata
- */
-export function getMasterRecordTypeId(objectInfo) {
-    if (objectInfo.recordTypeInfos) {
-        for (let rtId in objectInfo.recordTypeInfos) {
-            if (objectInfo.recordTypeInfos[rtId].master) {
-                return objectInfo.recordTypeInfos[rtId].recordTypeId;
-            }
-        }
-    }
-    return null;
-}
-
-/**
- * Returns the record's record type id
- * @param {Object} record SObject record
- */
-export function getRecordTypeId(record) {
-    return record.recordTypeId || null;
-}
